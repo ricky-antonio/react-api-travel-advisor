d71faeb260563d81996e1e2ae86bfe4574f49983
Ricardo Monterrosa <rickyantonio117@gmail.com>
2022-01-22 21:52:35 -0800

    Add header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { CssBaseline, Grid } from '@material-ui/core';
 
-import './App.css';
+import Header from './components/Header/Header';
+import Map from './components/Map/Map';
+import List from './components/List/List';
+
+// import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>working</h1>
-    </div>
-  );
+	return (
+		<>
+			<CssBaseline />
+			<Header />
+			<Grid container spacing={3} style={{ width: '100%' }}>
+				<Grid item xs={12} md={4}>
+					<List />
+				</Grid>
+				<Grid item xs={12} md={8}>
+					<Map />
+				</Grid>
+			</Grid>
+
+		</>
+	)
 }
 
 export default App;
